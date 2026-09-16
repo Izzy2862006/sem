5f728af2a5d978036db7233371ab3f99b09cd008
Izzy2862006 <cherrywaihlaing0010@gmail.com>
2026-09-16 13:39:40 +0630

    Updating App!

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class App
 {
-    public static void main(String[] args)
-    {
-        Connection con = null;
+    private Connection con = null;
 
+    public void connect()
+    {
         int retries = 10;
 
         for (int i = 0; i < retries; i++)
@@ -24,7 +24,7 @@ public class App
                         "example"
                 );
 
-                System.out.println("Successfully connected to database!");
+                System.out.println("Successfully connected");
                 break;
             }
             catch (SQLException e)
@@ -48,22 +48,37 @@ public class App
                 }
             }
         }
+    }
 
-        if (con == null)
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
         {
-            System.out.println("Could not connect to database.");
-            return;
+            try
+            {
+                con.close();
+                System.out.println("Database connection closed");
+            }
+            catch (SQLException e)
+            {
+                System.out.println("Error closing connection to database");
+                System.out.println(e.getMessage());
+            }
         }
+    }
 
-        try
-        {
-            con.close();
-            System.out.println("Database connection closed.");
-        }
-        catch (SQLException e)
-        {
-            System.out.println("Error closing database connection.");
-            System.out.println(e.getMessage());
-        }
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
