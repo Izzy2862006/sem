aa2515686f1b4247d3a68cec56896ffc1c2304df
Izzy2862006 <cherrywaihlaing0010@gmail.com>
2026-09-23 13:10:59 +0630

    Add salary report

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -241,6 +241,37 @@ public class App
     }
 
     /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format(
+                "%-10s %-15s %-20s %-8s",
+                "Emp No",
+                "First Name",
+                "Last Name",
+                "Salary"
+        ));
+
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format(
+                            "%-10s %-15s %-20s %-8s",
+                            emp.emp_no,
+                            emp.first_name,
+                            emp.last_name,
+                            emp.salary
+                    );
+
+            System.out.println(emp_string);
+        }
+    }
+
+    /**
      * Disconnect from the MySQL database.
      */
     public void disconnect()
@@ -269,7 +300,7 @@ public class App
 
         ArrayList<Employee> employees = a.getAllSalaries();
 
-        a.displayEmployees(employees);
+        a.printSalaries(employees);
 
         // Disconnect from database
         a.disconnect();
